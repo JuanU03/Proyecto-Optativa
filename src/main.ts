0c4a9e4b91905064fe7009140357fb008dab710d
Urriolabeitia Juan <juanurriolabeitia@gmail.com>
2025-09-24 15:23:33 -0300

    añadiendo swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
   console.log('NestJS corriendo en http://localhost:3000');
+  const config = new DocumentBuilder()
+   .setTitle('App example')
+   .setDescription('The app API description')
+   .setVersion('1.0')
+   .addTag('model')
+   .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+    await app.listen(3000);
 }
 bootstrap();
+
+
+
 
